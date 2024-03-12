7c6e080e48ec07ab8e3ffc0127c5152283f5f56c
POPs <148493288+probabilityofprofit@users.noreply.github.com>
2024-03-11 22:41:12 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,10 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
+[theme]\n\
+primaryColor = '#eb4034'\n\
+backgroundColor = '#021d24'\n\
+secondaryBackgroundColor = '#B9F1C0'\n\
+textColor = '#FFFFFF'\n\
+font = 'sans serif'\n\
 " > ~/.streamlit/config.toml
