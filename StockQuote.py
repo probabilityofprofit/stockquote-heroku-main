822b70ff933234ec64a72ff8f82ebf5907eed286
POPs <148493288+probabilityofprofit@users.noreply.github.com>
2024-03-12 21:44:24 -0700

    Update StockQuote.py

--- StockQuote.py
+++ StockQuote.py
@@ -33,7 +33,7 @@ from analysis_tab_utils import get_earnings_trend_data, display_earnings_trend_d
 from option_tab_utils import display_option_chain
 from stock_tickers import stock_options
 
-def modify_tag_content(tag_name, new_content, favicon_filename='PopFaviconBase.png'):
+def modify_tag_content(tag_name, new_content, favicon_filename=None):
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
     logging.info(f'editing {index_path}')
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
@@ -75,9 +75,10 @@ modify_tag_content('noscript', 'Best Stock Quotes ! Browse Thousands of Stocks F
 modify_tag_content('link', '', favicon_filename='PopFaviconBase.png')
 
 st.set_page_config(
-        page_title="Stock Quote",
-        page_icon = 'PopFaviconBase.png',
+    page_title="Stock Quote",
+    page_icon='PopFaviconBase1.png',
 )
+
 
 hide_decoration_bar_style = '''
     <style>
